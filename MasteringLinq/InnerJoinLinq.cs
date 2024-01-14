242150093f3f3d4a0e8b936ef83a23083dfd3104
Maharoz <mahruj66@gmail.com>
2024-01-15 00:49:41 +0600

    Group By added

---
+++ MasteringLinq/InnerJoinLinq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasteringLinq
+{
+    public class InnerJoinLinq
+    {
+        public void innerJoin()
+        {
+            var EmployeeWithAddress = Employee.GetAllEmployees().Join(
+              Address.GetAllAddresses(),
+              employee => employee.AddressId,
+              address => address.ID,
+              (employee, address) => new
+              {
+                  EmployeeName = employee.Name,
+                  AddressLine = address.AddressLine,
+              }
+              ).ToList();
+
+            foreach (var employee in EmployeeWithAddress)
+            {
+                Console.WriteLine($"{employee.AddressLine} ,{employee.EmployeeName}");
+            }
+        }
+    }
+}
--- MasteringLinq/Program.cs
+++ MasteringLinq/Program.cs
@@ -5,21 +5,6 @@
         static void Main(string[] args)
         {
 
-            var EmployeeWithAddress = Employee.GetAllEmployees().Join(
-                Address.GetAllAddresses(),
-                employee => employee.AddressId,
-                address => address.ID,
-                (employee, address) => new
-                {
-                    EmployeeName = employee.Name,
-                    AddressLine = address.AddressLine,
-                }
-                ).ToList();
-
-            foreach ( var employee in EmployeeWithAddress )
-            {
-                Console.WriteLine( $"{employee.AddressLine} ,{employee.EmployeeName}");
-            }
         }
     }
 }
