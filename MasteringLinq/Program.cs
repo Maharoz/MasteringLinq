ec098b0cd73d69091e5c4fc7d82dceac3c39157f
Maharoz <mahruj66@gmail.com>
2024-01-15 00:39:36 +0600

    Add project files.

---
+++ MasteringLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MasteringLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var EmployeeWithAddress = Employee.GetAllEmployees().Join(
+                Address.GetAllAddresses(),
+                employee => employee.AddressId,
+                address => address.ID,
+                (employee, address) => new
+                {
+                    EmployeeName = employee.Name,
+                    AddressLine = address.AddressLine,
+                }
+                ).ToList();
+
+            foreach ( var employee in EmployeeWithAddress )
+            {
+                Console.WriteLine( $"{employee.AddressLine} ,{employee.EmployeeName}");
+            }
+        }
+    }
+}
